92a3e149a64a7f9ae787f314d54e9a6686f4e3cf
ryuichi-hirano <s2122068@stu.musashino-u.ac.jp>
2024-01-17 23:37:22 +0900

    update library

--- build/lib/estimate_population/population_estimator.py
+++ build/lib/estimate_population/population_estimator.py
@@ -2,25 +2,25 @@ import math
 import csv
 
 
-def estimate_population_size(csv_file, sample_size, observed_unique_count, confidence_level=0.95, margin_of_error=0.05):
+def estimate_population_size_from_csv(csv_file, confidence_level=0.95, margin_of_error=0.05):
     """
-    サンプルサイズから母集団の数を推定します。
+    CSVファイルの行数をサンプルサイズとして、母集団の数を推定します。
 
-    :param sample_size: サンプルの行数
-    :param observed_unique_count: サンプル中のユニークな要素の数
+    :param csv_file: CSVファイルのパス
     :param confidence_level: 信頼度（デフォルトは95%）
     :param margin_of_error: 信頼区間の幅（デフォルトは5%）
     :return: 母集団の推定値と信頼区間
     """
-    
     try:
         with open(csv_file, 'r') as file:
             csv_reader = csv.reader(file)
-            sample_size = sum(1 for row in csv_reader)
+            # CSVファイルの行数をサンプルサイズとして使用
+            sample_size = sum(1 for _ in csv_reader)
     except FileNotFoundError:
         raise FileNotFoundError(f"File not found: {csv_file}")
+
     z_score = 0.5 * (1 + math.erf((1 - confidence_level) / math.sqrt(2)))
-    estimated_population = (observed_unique_count ** 2 / sample_size) * (1 / margin_of_error ** 2)
+    estimated_population = sample_size / margin_of_error ** 2
     lower_bound = estimated_population / (1 + (estimated_population - 1) / sample_size * z_score ** 2)
     upper_bound = estimated_population / (1 + (estimated_population - 1) / sample_size * z_score ** 2 / sample_size)
 
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='estimate_population',
-    version='0.1.0',
+    version='0.1.2',
     packages=find_packages(),
     install_requires=[],
     entry_points={},
